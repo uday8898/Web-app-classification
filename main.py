8a6d782f624f693c5f84405348a1b39ae74da3ed
UDAY GUPTA <91193540+uday8898@users.noreply.github.com>
2024-07-18 01:14:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ st.header('Please upload a chest X-ray image')
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-model = load_model('model\pneumonia_classifier.h5')
+model = load_model('model/pneumonia_classifier.h5')
 
 # load class names
-with open('model\labels.txt', 'r') as f:
+with open('model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
